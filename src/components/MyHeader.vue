ac18969486e4a7addf02e01d7a37d2a48836a2cf
Lone-DO <lotussythe@gmail.com>
2023-11-12 13:41:50 +0100

    Separate components into respective files

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header id="my-header">
+    <i class="fa-solid fa-plate-wheat"></i>
+    <h1>MealPrep.org</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+}
+</script>
+
+<style lang="scss">
+#my-header {
+  gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+}
+</style>
---
+++ src/components/MyNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav id="navigation">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default { 
+  name: 'MyNavigation',
+  components: { RouterLink },
+}
+</script>
+
+<style lang="scss">
+  #my-navigation {
+    grid-area: nav;
+  }
+</style>
